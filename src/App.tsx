79eccd25ad40a77cd6402b5f752bdc85a23e54ec
MaSteR_MinD1 <shehabbebo6060@gmail.com>
2022-11-28 10:13:56 +0200

    add successfully rendering the table content with task_id and the start and end time

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React, { useContext } from "react";
 import "./App.css";
 import Api, { User } from "@timetac/js-client-library";
 import { environment, authCredentials } from "./apiConfig";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { TtContextData } from "./context/TimeTrackingContext";
-import { TimeTrackingScreen } from "./components/TimeTrackings";
+import TimeTrackingView from "./components/TimeTrackings/view";
 function App() {
   const api = new Api(environment);
   const [usersTimeTrackingData, setUsersTimeTrackingData] =
@@ -12,11 +11,13 @@ function App() {
   const [userData, setUserData] = useState<User>();
 
   const [authed, setAuthed] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     if (!authed) {
       api.authentication.login(authCredentials).then((tokensData) => {
         api.authentication.setTokens(tokensData);
+        console.log("token dataaaaaaaaaaaa", tokensData);
         setAuthed(true);
       });
     }
@@ -24,20 +25,24 @@ function App() {
 
   useEffect(() => {
     if (authed && !userData) {
-      api.users.readMe().then((meData) => {
-        setUserData(meData);
+      setIsLoading(true);
+      api.users.readMe().then((Mydata: any) => {
+        setUserData(Mydata);
+        console.log(Mydata, "------------------");
       });
+
       api.timeTrackings.read().then((userTracking) => {
         setUsersTimeTrackingData(userTracking);
+        console.log("the data is", usersTimeTrackingData);
+        setIsLoading(false);
       });
     }
-  }, [authed, userData, api.users, usersTimeTrackingData]);
-  console.log("the data is", usersTimeTrackingData);
-  return (
-    <div className="App">
-      <TimeTrackingScreen />
-    </div>
-  );
+
+    console.log(userData, "------------------");
+    console.log("the auth is", authed);
+  }, [authed, userData, api.users]);
+
+  return <>{isLoading ? <h1>loading</h1> : <TimeTrackingView />}</>;
 }
 
 export default App;
